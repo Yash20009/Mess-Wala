bc719d41ece6ba7f2875c9596a4282f6d06def67
Yash20009 <132245887+Yash20009@users.noreply.github.com>
2025-05-06 21:38:56 +0530

    6 may 2025

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDY4ANbJYXrpzS3Nmt6G9y12tsskljzBdo",
+  authDomain: "mess-wala-2026.firebaseapp.com",
+  projectId: "mess-wala-2026",
+  storageBucket: "mess-wala-2026.firebasestorage.app",
+  messagingSenderId: "617677785056",
+  appId: "1:617677785056:web:921a8da764690d19d44d85"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { db };
+
